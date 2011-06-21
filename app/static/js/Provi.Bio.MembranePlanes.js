9e1ca63ca6d06bcca3b445130f88386571439511
alexrose <alexrose@alexmac.charite.de>
2011-06-21 16:49:23 +0200

    ADDED: mplane membrane planes are now depicted by the plane that intersects the current proteins bounding box

--- app/static/js/Provi.Bio.MembranePlanes.js
+++ app/static/js/Provi.Bio.MembranePlanes.js
@@ -52,7 +52,7 @@ Provi.Bio.MembranePlanes.MplaneWidget = function(params){
     this.applet = params.applet;
     this.color = "blue";
     this.translucency = 0.6;
-    this.size = 500;
+    this.size = -1;
     this.visibility = true;
     Widget.call( this, params );
     this.size_id = this.id + '_size';
@@ -68,12 +68,13 @@ Provi.Bio.MembranePlanes.MplaneWidget = function(params){
             '<label for="' + this.size_id + '">membrane plane size</label>' +
             '<select id="' + this.size_id + '" class="ui-state-default">' +
                 '<option value="1">hide</option>' +
+                '<option value="-1" selected="selected">boundbox</option>' +
                 '<option id="' + this.size_slider_option_id + '" value="1">slider</option>' +
                 '<option value="100">100</option>' +
                 '<option value="200">200</option>' + 
                 '<option value="300">300</option>' +
                 '<option value="400">400</option>' +
-                '<option value="500" selected="selected">500</option>' +
+                '<option value="500">500</option>' +
                 '<option value="600">600</option>' +
                 '<option value="700">700</option>' +
                 '<option value="800">800</option>' +
@@ -114,6 +115,7 @@ Provi.Bio.MembranePlanes.MplaneWidget.prototype = Utils.extend(Widget, /** @lend
             self.update_size_slider();
         }});
         $("#" + this.size_slider_id).mousewheel( function(event, delta){
+            event.preventDefault();
             self.size = Math.round(self.size + 20*delta);
             if(self.size > 1400) self.size = 1400;
             if(self.size < 1) self.size = 1;
@@ -144,8 +146,18 @@ Provi.Bio.MembranePlanes.MplaneWidget.prototype = Utils.extend(Widget, /** @lend
         if(this.visibility){
             var mp = this.dataset.data;
             var mp_f = mp.format_as_jmol_planes();
-            var s = 'draw plane' + this.id + '_1 color TRANSLUCENT ' + this.translucency + ' ' + this.color + ' plane ' + this.size + ' ' + mp_f[0] + '; draw plane' + this.id + '_2 color TRANSLUCENT ' + this.translucency + ' ' + this.color + ' plane ' + this.size + ' ' + mp_f[1] + ';';
-            s += 'draw dist arrow {' + mp.plane1[2].join(',') + '} {' + mp.plane2[2].join(',') + '} "' + mp.distance.toFixed(2) + ' A";';
+            var base_name = 'plane_' + this.id;
+            var color = 'color TRANSLUCENT ' + this.translucency + ' ' + this.color;
+            var base_plane = (this.size==-1) ?
+                ('intersection boundbox plane ') :
+                ('plane ' + this.size + ' ')
+            
+            var s = 'boundbox {*} OFF;';
+            s += 'draw ' + base_name + '_1 ' + color + ' ' + base_plane + mp_f[0] + ';';
+            s += 'draw ' + base_name + '_2 ' + color + ' ' + base_plane + mp_f[1] + ';';
+                
+            //s += 'draw dist arrow {' + mp.plane1[2].join(',') + '} {' + mp.plane2[2].join(',') + '} "' + mp.distance.toFixed(2) + ' A";';
+                
             this.applet.script(s);
         }else{
             this.applet.script('draw plane' + this.id + '_* off;');
