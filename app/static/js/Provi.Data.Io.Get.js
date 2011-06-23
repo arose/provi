8a6ac684f582eed6df2c9a9ea6632dd6ec6450e5
Alexander Rose <alexander.rose@weirdbyte.de>
2011-06-23 11:24:04 +0200

    ADD: example data can now be loaded via a json file instead of get parameters

--- app/static/js/Provi.Data.Io.Get.js
+++ app/static/js/Provi.Data.Io.Get.js
@@ -53,10 +53,39 @@ Provi.Data.Io.Get = $.extend(Provi.Data.Io.Get, /** @lends Provi.Data.Io.Get */ 
 		    var jw = new Provi.Jmol.JmolWidget({ parent: this.parent_id });
 		    jw_dict[i] = jw;
 		}
-		var params = $.extend( (data.params || {}), { applet: jw.applet, load_as: load_as } );
+		var params = $.extend( (data.params || {}), { applet: jw.applet, load_as: (data.load_as || 'new') } );
                 Provi.Data.Io.import_example( data.dir, data.filename, data.type, params );
 		
             });
+        }
+	
+	//example json url (eju)
+	if( $.query.get('example_json_url') ){
+	    console.log( $.query.get('example_json_url') );
+	    $.ajax({
+		url: $.query.get('example_json_url'),
+		data: {},
+		success: function(response){
+		    var jw_dict = {};
+		    $.each( response, function(i, data){
+			if(jw_dict[data.applet]){
+			    var jw = jw_dict[data.applet];
+			}else{
+			    var jw = new Provi.Jmol.JmolWidget({ parent: this.parent_id });
+			    jw_dict[i] = jw;
+			}
+			var params = $.extend( (data.params || {}), {
+			    applet: jw.applet,
+			    load_as: (data.load_as || 'new')
+			});
+			Provi.Data.Io.import_example( data.dir, data.filename, data.type, params );
+			
+		    });
+		},
+		error: function( jqXHR, textStatus, errorThrown ){
+		    console.log( jqXHR, textStatus, errorThrown );
+		}
+	    });
         }
 	
 	if( $.query.get('url') ){
