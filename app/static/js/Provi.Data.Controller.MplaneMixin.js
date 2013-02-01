118948390418cee3bbca760d21934f53343f6817
Alexander Rose <alexander.rose@weirdbyte.de>
2013-02-01 10:55:02 +0100

    FIX: mplane loading

--- app/static/js/Provi.Data.Controller.MplaneMixin.js
+++ app/static/js/Provi.Data.Controller.MplaneMixin.js
@@ -13,9 +13,12 @@ Provi.Data.Controller.MplaneMixin = {
     },
     init: function( params ){
         var self = this;
+        console.log('MPLANE init');
         this.retrieve_data( function(d){
+            console.log('MPLANE onload');
             self.set_data( new Provi.Bio.MembranePlanes.Mplane( d[0], d[1], d[2] ) );
             if( params.applet ){
+                console.log('MPLANEdfmgmsdpfgmsdpsd');
                 new Provi.Bio.MembranePlanes.MplaneWidget( $.extend( params, {
                     parent_id: Provi.defaults.dom_parent_ids.DATASET_WIDGET,
                     dataset: self
@@ -25,7 +28,15 @@ Provi.Data.Controller.MplaneMixin = {
         Provi.Data.Dataset.prototype.init.call(this, params);
     },
     retrieve_data: function( onload ){
-        var get_params = { 'id': this.server_id+'', 'data_action': 'get_planes' };
-        $.getJSON( '../../data/get/', get_params, onload );
+        console.log('MPLANE retrieve');
+        var get_params = { 'id': this.server_id+'', 'data_action': 'get_planes', 'session_id': $.cookie('provisessions') };
+        //$.getJSON( '../../data/get/', get_params, onload );
+        $.ajax({
+            dataType: "json",
+            url: '../../data/get/',
+            data: get_params,
+            success: onload,
+            error: function(e){ console.log(e); }
+        });
     }
 }
