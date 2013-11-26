88416ed906448897fb4394af7b7b86979f35c026
Alexander Rose <alexander.rose@weirdbyte.de>
2013-11-26 14:59:04 +0100

    UPDATE: Jmol

--- app/static/js/Provi.js
+++ app/static/js/Provi.js
@@ -73,7 +73,7 @@ var Jmol = {};
 
         // Jmol & Jalview init
         Provi.Jmol.init(
-            "../../jmol/current/68/", !$.query.get('unsigned')
+            "../../jmol/current/70/", !$.query.get('unsigned')
         );
         Provi.Jalview.init(
             "../../jalview/current/0/", !$.query.get('unsigned')
